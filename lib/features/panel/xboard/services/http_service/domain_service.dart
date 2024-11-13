dc372b5b2f6a3c9f2a720c937385d7dab0db1409
522507756 <522507756@qq.com>
2024-11-13 10:27:53 +0800

    Update domain_service.dart

--- lib/features/panel/xboard/services/http_service/domain_service.dart
+++ lib/features/panel/xboard/services/http_service/domain_service.dart
@@ -5,7 +5,7 @@ import 'package:http/http.dart' as http;
 
 class DomainService {
   static const String ossDomain =
-      'https://storage.googleapis.com/oss-clarity/config.json';
+      'https://dongjianwei.cn:8123/local/config.json';
 
 // 从返回的 JSON 中挑选一个可以正常访问的域名
   static Future<String> fetchValidDomain() async {
